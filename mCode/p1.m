c9e561a3775a1e9f3e883056a27a8424c427a3eb
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-05-18 12:19:00 +1000

    Update mCode/p1.m

--- mCode/p1.m
+++ mCode/p1.m
@@ -3,6 +3,7 @@ a=1;
 % Structs are defined using '.'
 Person1.FirstName = 'Jane';
 Person1.LastName = 'Doe';
+
 % A single struct can store different data types
 Person1.PassportNum = 12345;
 Person1.DOB = '01/01/2001';
