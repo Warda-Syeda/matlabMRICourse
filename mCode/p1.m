05c6bbdb8f14d6ef780a7962a434997308156b99
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-05-18 12:19:23 +1000

    Update mCode/p1.m

--- mCode/p1.m
+++ mCode/p1.m
@@ -7,8 +7,10 @@ Person1.LastName = 'Doe';
 % A single struct can store different data types
 Person1.PassportNum = 12345;
 Person1.DOB = '01/01/2001';
+
 % Funtion struct creates a structure array
 Person2 = struct('FirstName','John','LastName','Doe','PassportNum',67891,'DOB','30/01/2001');
+
 % An array of structures
 Directory(1).Person = Person1;
 Directory(2).Person = Person2;
