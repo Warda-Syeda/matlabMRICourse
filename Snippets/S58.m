c37c754d68b9be0d8bd9f02c1a0854903f3d6816
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-06-22 09:56:53 +1000

    Update Snippets/S58.m

--- Snippets/S58.m
+++ Snippets/S58.m
@@ -1,7 +1,8 @@
 [humanBrain_L_white.faces,humanBrain_L_white.vertices]=ply_read('humanBrain.L.white.ply','tri');
-load('humanBrainCurvature');
+load('humanBrainCurvature_L_white');
 figure;
 patch('faces', humanBrain_L_white.faces', 'vertices', humanBrain_L_white.vertices','FaceVertexCData', humanBrainCurvature,'edgecolor','none','FaceColor','interp', 'FaceColor','interp','FaceLighting', 'gouraud','AmbientStrength', 0.9);view(-90,0);
 camlight;
+colormap('jet');
 axis equal; axis off;
 title('Fig.8')
