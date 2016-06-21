ff5b50b3bc32175ff7f9f500c6693d720508c7f4
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-06-22 09:47:48 +1000

    Update Snippets/S43.m

--- Snippets/S43.m
+++ Snippets/S43.m
@@ -1,8 +1,6 @@
 % Create a 40x40x40 cube in the center of a 140x140x140 frame 
 frameDim=140;       % declaring frame dimensions
 
-% Make a 3D grid
-[Y X Z] = meshgrid(1:frameDim,1:frameDim,1:frameDim);  %matlab saves in a different order so X must be Y
 
 % Define cube center aligned to the center of the frame
 centerX = frameDim/2;
@@ -20,3 +18,6 @@ tempImage(centerX-xCubeDim/2:centerX+xCubeDim/2, ...
 
 % Create 3D cube
 myCube = tempImage;
+
+% Make a 3D grid
+[Y X Z] = meshgrid(1:frameDim,1:frameDim,1:frameDim);  %matlab saves in a different order so X must be Y
