e31bd44cb70f20977f62fa9183d7acb08e2bc0c2
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-06-22 09:59:35 +1000

    Update Snippets/S51.m

--- Snippets/S51.m
+++ Snippets/S51.m
@@ -1,6 +1,6 @@
 figure;
 patch('Faces',myCubeSurface.faces,'Vertices',myCubeSurface.vertices,'FaceVertexCData',colorCode,'FaceColor','interp','EdgeColor','none');
-% myCubeHandle=patch('Faces',myCubeSurface.faces,'Vertices',myCubeSurface.vertices,'FaceVertexCData',colorCode,'FaceColor','interp','EdgeColor','none','EdgeLightin','flat');
+% patch('Faces',myCubeSurface.faces,'Vertices',myCubeSurface.vertices,'FaceVertexCData',colorCode,'FaceColor','interp','EdgeColor','none','EdgeLightin','flat');
 view([45,20])
 camlight(-10,10)
 axis equal
