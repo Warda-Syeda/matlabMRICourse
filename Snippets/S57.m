979b1a1c9f92962f5124d01efaa9dd4bd9cf87b6
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-06-22 09:52:54 +1000

    Update Snippets/S57.m

--- Snippets/S57.m
+++ Snippets/S57.m
@@ -1,6 +1,6 @@
 [femurSurface.faces,femurSurface.verices]=stlread('femurSurface.stl');
 figure
-patch('Faces',femurSurface.faces,'Vertices',femurSurface.vertices,'FaceColor','y');
+patch('Faces',femurSurface.faces,'Vertices',femurSurface.vertices,'FaceColor','y', 'EdgeColor', 'none');
 camlight(-10,10)
 axis equal
 title('Fig.7')
